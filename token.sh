4a7cec191fc342966691f5583f46f76332423423
Assen Sharlandjiev <assen.sharlandjiev@gmail.com>
2024-11-19 00:26:22 +0200

    ...

--- token.sh
+++ token.sh
@@ -1,2 +1,4 @@
-echo `kubectl get secret admin-user -n kube-system -o jsonpath={".data.token"} | base64 -d`
+#kubectl get secret -n kube-system microk8s-dashboard-token
+#echo `kubectl -n kube-system get secret microk8s-dashboard-token -o jsonpath={".data.token"} | base64 -d`
 
+echo `kubectl get secret admin-user -n kube-system -o jsonpath={".data.token"} | base64 -d`
